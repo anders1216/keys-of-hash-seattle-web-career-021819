2f81e1b43d56ca540e71c44ee5fb6d227e09958f
Anders Hornor <anders.hornor@gmail.com>
2019-02-06 17:01:34 -0800

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -7,9 +7,9 @@ class Hash # I guess this works
   def keys_of(*arguments)
     key_arr = []
     arguments.each do |idek| #iterates over arguments if arguments>1 and sets each argument up as a variable to be called on in the next iterator. Tried arguments = idek, didnt work for multiple arguments.
-      self.each do |thingIwant, maybe_idek| #putting in one argument[?] here outputs an array that is the key/value pair from self that is being iterated over by 'each' instead of two arguments which would seperate the key/value pair into the corresponding argument.
-        if maybe_idek == idek
-          key_arr << thingIwant
+      self.each do |thingIwant| #putting in one argument[?] here outputs an array that is the key/value pair from self that is being iterated over by 'each' instead of two arguments which would seperate the key/value pair into the corresponding arguments. either way works but the following if statment must reflect the outputs.
+        if thingIwant.include?(idek)
+          key_arr << thingIwant[0]
         end
         end
       end
