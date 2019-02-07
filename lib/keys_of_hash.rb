9c32b20d74ed839848c4b551de5a5ed38a8e7019
Anders Hornor <anders.hornor@gmail.com>
2019-02-06 16:56:30 -0800

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -6,13 +6,13 @@ require 'pry'
 class Hash # I guess this works
   def keys_of(*arguments)
     key_arr = []
-    arguments.each do |idek| # Sets up the the key/value backwards comparison I believe.
-      self.each do |thingIwant| #I'm still unsure about self... so this basically reaches outside of the method to reference the hash that the method is being called on and iterates through the hash?
-        if thingIwant.include?(idek)
-          key_arr << thingIwant[0]
+    arguments.each do |idek| #iterates over arguments if arguments>1 and sets each argument up as a variable to be called on in the next iterator. Tried arguments = idek, didnt work for multiple arguments.
+      self.each do |thingIwant, maybe_idek| #putting in one argument[?] here outputs an array that is the key/value pair from self that is being iterated over by 'each' instead of two arguments which would seperate the key/value pair into the corresponding argument.
+        if maybe_idek == idek
+          key_arr << thingIwant
+        end
         end
       end
-    end
     key_arr
   end
 end
