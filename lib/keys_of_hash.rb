4ccc260267305eae2f45ea516128337553e9b558
Anders Hornor <anders.hornor@gmail.com>
2019-02-06 16:13:20 -0800

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,18 @@
-class Hash
-  def keys_of(arguments)
-    # code goes here
+# This method should take an undefined number of arguments, using the splat
+# operator, and return an array with every key from the hash whose value matches
+# the value(s) given as an argument.
+require 'pry'
+
+class Hash # I guess this works
+  def keys_of(*arguments)
+    key_arr = []
+    arguments.each do |idek| # Sets up the the key/value backwards comparison I believe.
+      self.each do |thingIwant| #I'm still unsure about self... so this basically reaches outside of the method to reference the hash that the method is being called on and iterates through the hash?
+        if thingIwant.include?(idek)
+          key_arr << thingIwant[0]
+        end
+      end
+    end
+    key_arr
   end
 end
